c96b9c81fd8127759a7ffeb7c01d508c06f4ee15
Shehrozkhannn <sharozekkhan@gmail.com>
2024-10-11 15:38:14 +0500

    Deploy changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class DataService {
   // private apiUrl = 'http://localhost:3000/api'; 
-  private apiUrl = 'currency-api-backend.vercel.app'; 
+  private apiUrl = ''; 
 
   constructor(private http: HttpClient) { }
 
